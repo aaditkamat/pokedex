499c9f050cddb94455152962a29ae2f60ebbb9cc
Aadit Kamat <aadit.k12@gmail.com>
2020-08-16 21:07:35 +0800

    Create html base with bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <React.Fragment>
+      <header className="app-heading">
+          <h1> Pokedex </h1>
+          <div className="container">
+            <div className="row">
+              <div className="search-box column">
+                <input className="search" placeholder="Search Pokemon" />
+                <div className="pokemon-info">
+                  <img src="" alt="Bulbusaur" className="pokemon-image"/>
+                  <span className="pokemon-name"> Bulbusaur </span>
+                </div>
+              </div>
+              <div className="welcome-box column">
+                Welcome to the Pokedex
+              </div>
+            </div>
+          </div>
+      </header>
+    </React.Fragment>
+  );
 }
